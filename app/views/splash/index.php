e6a1e62b3087cf400373892fce934f27cd334832
Jakob Buis <jpabuis@gmail.com>
2014-02-02 21:26:34 +0100

    removed not valid HTML input property

--- app/views/splash/index.php
+++ app/views/splash/index.php
@@ -2,6 +2,6 @@
 <p>Welkom bij ZeusMatch, jouw plek om nieuwe (leuke) zusjes te ontmoeten!</p>
 <p>
 <form action="/login" method="GET" data-ajax="false">
-    <input value="Login met Facebook" type="submit" data-theme="c" href="/login">
+    <input value="Login met Facebook" type="submit" data-theme="c">
 </form>
 </p>

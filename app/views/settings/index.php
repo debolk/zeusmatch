3504cad5cc9f57f7226d221c032e89ce52b231e6
Jakob Buis <jpabuis@gmail.com>
2014-02-02 21:24:53 +0100

    simpler solution for logging out of the app

--- app/views/settings/index.php
+++ app/views/settings/index.php
@@ -61,9 +61,9 @@
 <p>
     Als je wilt, kun je hier uitloggen van de app. Dit heeft geen effect op de data die wordt bewaard. Je blijft ingelogd op Facebook. Normaal gesproken heb je deze optie niet nodig.
 </p>
-<p>
-    <input data-icon="power" value="Uitloggen van ZeusMatch" type="button" data-theme="c" id="logout">
-</p>
+<form action="/logout" method="GET" data-ajax="false">
+    <input data-icon="power" value="Uitloggen van ZeusMatch" type="button" data-theme="c">
+</form>
 <h2>Profiel verwijderen</h2>
 <p>
     Weet je zeker dat je je profiel wilt verwijderen? Alle data (je foto's, likes, matches) wordt onmiddelijk verwijderd en kan niet meer worden teruggehaald. Je kan deze stap niet ongedaan maken.
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,2 @@
 $(document).ready(function(){
-    // Settings: Logout from the app
-    $('#logout').click(logout);
 });
-
-function logout(event)
-{
-    event.preventDefault();
-    window.location = '/logout';
-}

741d503bee3332e445d139586f917f130365092e
Jakob Buis <jpabuis@gmail.com>
2014-02-02 20:59:46 +0100

    added bottom menu with privacy and contact to the splash screen

--- app/views/layouts/splash.php
+++ app/views/layouts/splash.php
@@ -3,4 +3,12 @@
             <div role="main" class="ui-content">
                 <?php echo $content; ?>
             </div>
+<div data-role="footer" data-position="fixed">
+    <div data-role="navbar">
+        <ul>
+            <li><a href="/privacy" data-icon="power">Privacy</a></li>
+            <li><a href="/contact" data-icon="mail">Contact</a></li>
+        </ul>
+    </div>
+</div>
 <?php echo View::make('layouts/_footer'); ?>

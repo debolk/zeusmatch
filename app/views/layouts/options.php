717fcd0d9ccb5217147e74f744695a0ec0f5293c
Jakob Buis <jpabuis@gmail.com>
2014-02-02 21:03:43 +0100

    hide the settings button in the lower menu when the user is not logged in (good for UI consistency)

--- app/views/layouts/options.php
+++ app/views/layouts/options.php
@@ -7,7 +7,9 @@
 <div data-role="footer" data-position="fixed">
     <div data-role="navbar">
         <ul>
-            <li><a href="/settings" data-icon="gear" <?php echo Request::is('settings') ? 'class="ui-btn-active"': null; ?>>Instellingen</a></li>
+            <?php if (Auth::check()): ?>
+                <li><a href="/settings" data-icon="gear" <?php echo Request::is('settings') ? 'class="ui-btn-active"': null; ?>>Instellingen</a></li>
+            <?php endif; ?>
             <li><a href="/privacy" data-icon="power" <?php echo Request::is('privacy') ? 'class="ui-btn-active"': null; ?>>Privacy</a></li>
             <li><a href="/contact" data-icon="mail" <?php echo Request::is('contact') ? 'class="ui-btn-active"': null; ?>>Contact</a></li>
         </ul>

332488df9b043add6ca369dd1b833f7ea212eaff
Jakob Buis <jpabuis@gmail.com>
2014-02-03 22:27:52 +0100

    improved display of popup on succesful contact form submission

--- app/views/contact/_thanks.php
+++ app/views/contact/_thanks.php
@@ -2,7 +2,9 @@
   <div data-role="header">
       <h1>Bericht verzonden</h1>
   </div>
-  <div data-role="content">
-      <p>Je bericht is verzonden. Indien nodig nemen we contact met je op.</p>
+  <div role="main" class="ui-content">
+      <p>Je bericht is verzonden. <br>
+      Indien nodig nemen we contact met je op.</p>
+      <a href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-rel="back">Okee</a>
   </div>
 </div>

60ebcb9c794ef0101cba0a490416671eb2a521cb
Jakob Buis <jpabuis@gmail.com>
2014-02-22 18:03:23 +0100

    uncommented code that was disabled for debugging

--- app/controllers/MatchController.php
+++ app/controllers/MatchController.php
@@ -36,9 +36,9 @@ class MatchController extends BaseController
         $current_user = Auth::user();
 
         // A user cannot (dis)like itself
-        // if ($current_user->id === $other_id) {
-        //     App::abort(403, 'You cannot (dis)like yourself');
-        // }
+        if ($current_user->id === $other_id) {
+            App::abort(403, 'You cannot (dis)like yourself');
+        }
 
         // Write the like
         $like = Like::firstOrCreate(['from_id' => $current_user->id, 'to_id' => $other_id]);
